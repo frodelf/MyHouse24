388afae4cbc7ca9fb7b0dc9b0dd39958b2b67367
OkeNNV <OkeNNV@gmail.com>
2023-07-14 06:21:28 +0300

    Roles, admin auth

---
+++ src/main/java/com/avada/myHouse24/service/registration/ValidationResponse.java
@@ -0,0 +1,19 @@
+//package com.avada.myHouse24.service.registration;
+//
+//import lombok.Data;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Data
+//public class ValidationResponse {
+//    private boolean valid;
+//    private Map<String, String> errors;
+//
+//    public void addError(String fieldName, String errorMessage) {
+//        if (errors == null) {
+//            errors = new HashMap<>();
+//        }
+//        errors.put(fieldName, errorMessage);
+//    }
+//}
