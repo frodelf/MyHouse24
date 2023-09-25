703a6aceb9847efc3d43eb4d3153b58a4f8d5be1
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-09-25 17:55:18 +0300

    test

--- src/main/java/com/avada/myHouse24/config/WebConfig.java
+++ src/main/java/com/avada/myHouse24/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceHandler("/files/**")
                 .addResourceLocations("classpath:/static/files/");
         registry
-                .addResourceHandler("/aadminlte/**")
+                .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:/var/photos/");
     }
     @Override
