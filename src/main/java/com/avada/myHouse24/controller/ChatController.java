a5bc6c3d0c0b9f6f65455cba7788b8b6d0956b7c
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-10-13 00:05:01 +0300

    rabbitmq start

--- src/main/java/com/avada/myHouse24/controller/ChatController.java
+++ src/main/java/com/avada/myHouse24/controller/ChatController.java
@@ -4,12 +4,17 @@ import com.avada.myHouse24.entity.Chat;
 import com.avada.myHouse24.services.impl.AdminServiceImpl;
 import com.avada.myHouse24.services.impl.HouseServiceImpl;
 import com.avada.myHouse24.services.impl.ChatServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
@@ -22,24 +27,25 @@ public class ChatController {
     private final ChatServiceImpl messageService;
     private final HouseServiceImpl houseService;
     private final SimpMessagingTemplate messagingTemplate;
+    private final RabbitTemplate template;
 
-    @MessageMapping("/application")
-    public void send(final Chat chat) throws Exception {
-        Chat res = new Chat();
-        res.setDate(LocalDate.now());
-        res.setFromName(adminService.getAuthAdmin().getFirstName());
-        res.setIsUser(false);
-        res.setFromId(adminService.getAuthAdmin().getId());
-        res.setHouse(houseService.getById(chat.getFromId()));
-        res.setText(chat.getText());
-        messageService.save(res);
-        res.setHouse(null);
-        messagingTemplate.convertAndSend("/all/messages/"+ chat.getFromId(), res);
-    }
 
     @GetMapping("/chat/getAll/{page}/{houseId}")
     @ResponseBody
     public List<Chat> getComments (@PathVariable("page")int page, @PathVariable("houseId")long houseId){
         return messageService.getAll(page, houseId);
+    }
+
+    @GetMapping("/admin/rabbit/message")
+    public ResponseEntity<String> rabbitMessage(@RequestParam("text")String message, @RequestParam("house")Long house) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Chat chat = new Chat();
+        chat.setText(message);
+        chat.setFromName(adminService.getAuthAdmin().getFirstName());
+        chat.setFromId(house);
+        chat.setIsUser(false);
+        template.setExchange("common-exchange");
+        template.convertAndSend(objectMapper.writeValueAsString(chat));
+        return ResponseEntity.ok("Message sent to queue");
     }
 }
---
+++ src/main/java/com/avada/myHouse24/controller/RabbitMqListener.java
@@ -0,0 +1,38 @@
+package com.avada.myHouse24.controller;
+
+import com.avada.myHouse24.entity.Chat;
+import com.avada.myHouse24.services.impl.AdminServiceImpl;
+import com.avada.myHouse24.services.impl.ChatServiceImpl;
+import com.avada.myHouse24.services.impl.HouseServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@EnableRabbit
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class RabbitMqListener {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final HouseServiceImpl houseService;
+    private final AdminServiceImpl adminService;
+    private final ChatServiceImpl chatService;
+    @RabbitListener(queues = "myQueue2")
+    public void processMyQueue(String string) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Chat chat = objectMapper.readValue(string, Chat.class);
+        long houseId = chat.getFromId();
+        chat.setFromId(adminService.getAuthAdmin().getId());
+        chat.setDate(LocalDate.now());
+        messagingTemplate.convertAndSend("/all/messages/1", chat);
+        chat.setHouse(houseService.getById(houseId));
+        chatService.save(chat);
+    }
+}
